150213859865ea565d6d429c726cd7996961125c
Jesus Rios <jmriosal@gmail.com>
2026-02-16 14:20:14 -0500

    Update __init__.py

--- src/peft_sparta/__init__.py
+++ src/peft_sparta/__init__.py
@@ -1,4 +1,4 @@
-from .sparta_model import SpaRTAforSequenceClassification, SpaRTAforCausalLM
+from .adapted_models import SpaRTAforSequenceClassification, SpaRTAforCausalLM
 from .sparta import SpaRTA
 
 __all__ = ["SpaRTAforSequenceClassification", "SpaRTAforCausalLM", "SpaRTA"]
