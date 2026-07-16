2672a486174529939ca3964fe800bcaaea1baf69
Jesus Rios <jriosal@us.ibm.com>
2026-07-16 15:24:37 -0400

    plots modularized changes

--- src/peft_sparta/trainer/sft/sft.py
+++ src/peft_sparta/trainer/sft/sft.py
@@ -2,7 +2,6 @@ import os
 import yaml
 import math
 import torch
-import matplotlib.pyplot as plt
 from .data_processor import DataProcessor
 from ...metrics import mcc, f1_score
 
@@ -460,73 +459,18 @@ class SFT:
 
 
     def plot_stats(self, fname_prefix=''):
-        
-        fpath_prefix = os.path.join(self.config['output_dir'], fname_prefix)
-        
+
+        from . import plots
+
         if self.task == 'SEQ_CLS':
             num_classes = self.model.num_labels # self.train_dataloader.dataset.features['labels'].num_classes
             loss_baseline = -math.log(1 / num_classes)
         else:
             loss_baseline = None
-        
-        plot(self.stats['lr'],
-             title = 'Learning Rate',
-             save_file = fpath_prefix + 'learning_rate.pdf')
 
-        train_loss = self.stats['train_loss']
-        if self.val_dataloader:
-            val_loss = {'x': self.stats['val_step'], 'y': self.stats['val_loss']}
-            plot(train_loss, w = self.config['logging_freq'],
-                 baseline = loss_baseline,
-                 ts2 = val_loss, ts_name = 'train', ts2_name = 'val',
-                 anchor = val_loss['y'][0], # pre-trained performace at x=0
-                 title = 'Loss',
-                 save_file = fpath_prefix + 'loss.pdf')
-            if self.task == 'SEQ_CLS':
-                plot(self.stats['val_acc'], x_start = 0, # w = 1,
-                     baseline = 1.0,
-                     title = 'Validation Accuracy',
-                     save_file = fpath_prefix + 'val_acc.pdf')
-        else:
-            plot(train_loss, w = self.config['logging_freq'],
-                 baseline = loss_baseline,
-                 title = 'Training Loss',
-                 save_file = fpath_prefix + 'train_loss.pdf')
-
-        if self.config['max_grad_norm']:
-            max_grad_norm = self.config['max_grad_norm']
-            plot(self.stats['grad_norm'], # w = 1, 
-                 baseline = max_grad_norm if max_grad_norm < float('inf') else 0, 
-                 title = 'Gradient Norm',  
-                 save_file = fpath_prefix + 'grad_norm.pdf')
-
-
-
-
-def plot(ts, x_start=1, w=None, baseline=None, title=None, save_file=None,
-         ts2=None, ts_name=None, ts2_name=None, anchor=None):
-    fig, ax = plt.subplots(figsize=(8,5))
-    if w: # rolling avg window
-        y = [sum(ts[i:i+w])/w for i in range(len(ts) - w + 1)]
-        x = list(range(x_start + w - 1, x_start + len(ts))) # last batch-step of each window
-    else:
-        y = ts
-        x = list(range(x_start , x_start + len(ts)))
-    if anchor:
-        assert x_start == 1
-        x, y = [0] + x, [anchor] + y # line segment from x=0 to x=w
-    ax.plot(x, y, label=ts_name);
-    if baseline:
-        ax.hlines(baseline, xmin=0, xmax=x[-1], color='r')
-    if ts2:
-        ax.plot(ts2['x'], ts2['y'], label=ts2_name)
-        ax.legend();
-    ax.set_ylim(bottom=0);
-    ax.set_title(title);
-    if save_file:
-        plt.savefig(save_file, bbox_inches='tight')
-        plt.close()
-    else:
-        plt.show()
+        plots.plot_stats(
+            self.stats, self.config, loss_baseline,
+            fname_prefix=fname_prefix
+        )
 
 
